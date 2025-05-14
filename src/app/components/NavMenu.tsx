63c42835e1f1d6336fb2a7857b731372169f6a72
Daniel <nguyendaniel1312@gmail.com>
2025-05-13 23:13:15 -0700

    Extra changes for deploy

--- src/app/components/NavMenu.tsx
+++ src/app/components/NavMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
 import {
