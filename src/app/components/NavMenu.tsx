1adc0370ac12ddec364bf950e5a5d9f2a27d4739
Daniel <nguyendaniel1312@gmail.com>
2025-05-20 01:05:19 -0700

    Update navbar for phone sizes
    Wasn't showing sign out button before

--- src/app/components/NavMenu.tsx
+++ src/app/components/NavMenu.tsx
@@ -28,7 +28,7 @@ export default function NavMenu() {
   const manageRef = useRef<HTMLDivElement>(null);
   const { data: session } = useSession();
   const [isOpen, setIsOpen] = useState(false);
-  const { privateMode, setPrivateMode } = usePrivateMode();
+  const [privateMode, setPrivateMode] = useState(false);
 
   /* Close modals when clicking outside */
 
@@ -106,13 +106,13 @@ export default function NavMenu() {
               alt="TwinMind Logo"
               width={150}
               height={150}
-              className="mx-auto translate-y-10"
+              className="mx-auto md:translate-y-10 translate-y-0"
             />
           </a>
 
           {/* Profile */}
           {session?.user && (
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 -translate-y-10 md:translate-y-0">
               {session.user.image && (
                 <Image
                   src={session.user.image}
@@ -135,7 +135,7 @@ export default function NavMenu() {
           )}
 
           {/* Settings */}
-          <div>
+          <div className="md:translate-y-0 -translate-y-10">
             <p className="text-gray-300 mb-2 font-semibold">Settings</p>
             <div className="flex flex-col gap-2 ml-5 w-48">
               {/* Fetches personalization from database before opening modal */}
@@ -182,7 +182,7 @@ export default function NavMenu() {
           </div>
 
           {/* Support */}
-          <div>
+          <div className="md:translate-y-0 -translate-y-10">
             <p className="text-gray-300 mb-2 font-semibold">Support</p>
             <div className="flex flex-col gap-2 ml-5 w-48">
               <a
@@ -207,7 +207,7 @@ export default function NavMenu() {
           </div>
 
           {/* Other */}
-          <div>
+          <div className="md:translate-y-0 -translate-y-10">
             <p className="text-gray-300 mb-2 font-semibold">Other</p>
             <div className="flex flex-col gap-2 ml-5 w-48">
               <a
@@ -234,7 +234,7 @@ export default function NavMenu() {
           {/* Sign out */}
           <button
             onClick={() => signOut()}
-            className="mt-4 px-10 py-2 bg-[#ff454b] hover:bg-red-600 rounded-full flex items-center justify-center gap-2 transition-all duration-300 cursor-pointer"
+            className="mt-4 px-10 py-2 bg-[#ff454b] hover:bg-red-600 rounded-full flex items-center justify-center gap-2 transition-all duration-300 cursor-pointer md:translate-y-0 -translate-y-10"
           >
             <LogOut />
             Sign out
